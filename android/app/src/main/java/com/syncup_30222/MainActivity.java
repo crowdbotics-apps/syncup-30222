e3ca9b65d94c57f61acc627d1ddd519ed987bba0
Crowdbotics <team@crowdbotics.com>
2021-08-31 16:16:28 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/syncup_30222/MainActivity.java
@@ -0,0 +1,15 @@
+package com.syncup_30222;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "syncup_30222";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "syncup_30222Identifier",
+  url: "https://syncup_30222Identifier.botics.co",
+  api: "https://syncup_30222Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
